b4bb96775527d6031af99ece65f32b3b6b1402d8
Christopher Gottshalk Jr <christophergottshalkjr@gmail.com>
2019-12-11 22:34:54 -0500

    Models setup and server running. Added Photos.

---
+++ models/Article.js
@@ -0,0 +1,44 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var ArticleSchema = new Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+
+    link: {
+        type: String,
+        required: true,
+        unique: true
+    },
+
+    image: {
+        type: String,
+        required: true
+    },
+
+    saved: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+
+    writer: {
+        type: String,
+        required: true
+    },
+
+    comment: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Note"
+        }
+    ]
+});
+
+var Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
---
+++ server.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Routes
+require("./routes/apiRoutes");
+require("./routes/htmlRoutes");
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+// Start the server
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
+
+
